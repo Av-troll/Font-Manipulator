8ced7f5b1808f01a5154121f8500d4b415df750e
AvniBookworm4ever <79648896+Av-troll@users.noreply.github.com>
2021-11-23 16:50:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    
+    var video = createCapture(VIDEO);
+    video.size(550,500);
+    
+    poseNet = ml5.poseNet(video,modelLoaded);
+     poseNet.on('pose',gotPoses);
+    }
+    
+    function modelLoaded(){
+        console.log("poseNet is initialized");
+    }
+    
+    function draw(){
+    background("turquoise");
+    }
+    
+    function gotPoses(results){
+      if(results.length > 0){
+          console.log(results);
+      }
+    }
